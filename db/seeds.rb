4a53b560b28d52a6c0c8718f012123bd8689b2b6
joao mesquita <jpmesquita29085@hotmail.com>
2024-05-22 21:46:27 +0100

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,20 @@
 #
 # Example:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#   ['Action', 'Comedy', 'Drama', 'Horror'].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'belgian' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'french' }
+pica_pau_amarelo = { name: 'Pica Pau Amarelo', address: '50B Paraíso do Sol, Rio de Janeiro E4 6FT', category: 'chinese' }
+ananas = { name: 'Ananás', address: 'Rua dá-me um abraço, Porto 4150-500', category: 'italian' }
+tc = { name: 'TC', address: 'World of wine Street, Gaia R1 8PQ', category: 'japanese' }
+
+[dishoom, pizza_east, pica_pau_amarelo, ananas, tc].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
